cc4077964e7db343cb168adc9d41dfc689d591a9
Your <your.email@example.com>
2025-07-23 12:48:08 +0100

    fix_supabase_access

--- Downloads/mama-s-porfolio-real-main/mama-s-porfolio-real-main/lib/supabase.ts
+++ Downloads/mama-s-porfolio-real-main/mama-s-porfolio-real-main/lib/supabase.ts
@@ -11,8 +11,8 @@ export const createServerClient = () => {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        async get(name: string) {
-          return (await cookieStore.get(name))?.value;
+        get(name: string) {
+          return cookieStore.get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
           cookieStore.set({ name, value, ...options });
